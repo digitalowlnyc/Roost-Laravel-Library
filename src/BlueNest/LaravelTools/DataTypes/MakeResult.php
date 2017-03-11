4db3f9486187978eedca8ba309733952586ef175
Bryan <heliosbryan@gmail.com>
2017-03-11 12:50:36 -0500

    Add MakeResult

---
+++ src/BlueNest/LaravelTools/DataTypes/MakeResult.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Creator: Bryan Mayor
+ * Company: Blue Nest Digital, LLC
+ * License: (All rights reserved)
+ */
+
+namespace BlueNest\LaravelTools\DataTypes;
+
+class MakeResult {
+    static function success() {
+        return new Result(1);
+    }
+
+    static function failure() {
+        return new Result(0);
+    }
+}
