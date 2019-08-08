409e880e42e6cf239f06f30ed5f9b1a2a5b90a8d
Bryan <bryan@bluenestdigital.com>
2019-08-07 23:33:32 -0400

    Add heartbeats command

--- src/Roost/LaravelTools/LaravelToolsServiceProvider.php
+++ src/Roost/LaravelTools/LaravelToolsServiceProvider.php
@@ -3,9 +3,17 @@
 namespace Roost\LaravelTools;
 
 use Illuminate\Support\ServiceProvider;
+use Roost\LaravelTools\Commands\Deployment\ApplicationCheckHeartbeatsCommand;
+use Roost\LaravelTools\Commands\Deployment\ApplicationTriggerHeartbeatsCommand;
 
 class LaravelToolsServiceProvider extends ServiceProvider {
 	public function boot() {
 		$this->loadMigrationsFrom(__DIR__.'/../../../database');
+
+		if ($this->app->runningInConsole()) {
+			$this->commands([
+				ApplicationTriggerHeartbeatsCommand::class
+			]);
+    }
 	}
 }
